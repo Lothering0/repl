db833fb2381af1ae8f00294b9be1deff412dbb19
Lothering0 <danil2005232@gmail.com>
2024-01-04 00:30:21 +0300

    Vim

--- index.js
+++ index.js
@@ -3,23 +3,45 @@ const util = require("util");
 const path = require("path");
 
 const package = require("./package.json");
+const readlineVim = require("readline-vim");
 const dayjs = require("dayjs");
 
 require("@colors/colors");
 require("./fp.js");
 
+/** @constant */
 const icons = {
   js: "",
-  prompt: ">=>",
+  prompt: {
+    insert: ">=>",
+    normal: "<=<",
+  },
   output: "->",
   node: "",
+};
+
+/** @constant */
+const colors = {
+  prompt: {
+    insert: "green",
+    normal: "cyan",
+  },
 };
 
 console.log("");
 const nodeVersion = "".green + (" " + icons.node + "  " + process.version + " ").brightWhite.bgGreen + "".green;
 console.log(nodeVersion + "  " + package.version);
 
-const prompt = icons.js.yellow + " " + icons.prompt.green + " ";
+/**
+ * @param {keyof typeof icons["prompt"]} mode
+ * @returns {string}
+ */
+const getPrompt = (mode) => {
+  const promptColor = colors.prompt[mode];
+  return icons.js.yellow + " " + icons.prompt[mode][promptColor] + " ";
+};
+
+const prompt = getPrompt("insert");
 
 const local = repl.start({
   prompt,
@@ -34,6 +56,16 @@ local.setupHistory(path.resolve(__dirname, "./history.txt"), (error) => {
   if (error) throw error;
 });
 
-// local.displayPrompt(true);
-
 local.context.dayjs = dayjs;
+
+const vim = readlineVim(local);
+
+vim.events.on("normal", () => {
+  local.setPrompt(getPrompt("normal"));
+});
+
+vim.events.on("insert", () => {
+  local.setPrompt(getPrompt("insert"));
+});
+
+// TODO: https://stackoverflow.com/questions/62323316/how-to-modify-cursor-line-in-node-js-readline
